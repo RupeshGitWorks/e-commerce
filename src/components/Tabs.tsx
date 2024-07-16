0c38ddfd1f06f679ce98732966b882b1fd6bde14
Rupesh Singh <rupesh.singh@teknopoint.local>
2024-07-16 15:07:04 +0530

    created Folder for layout, components and pages also added Header and Tabs component

---
+++ src/components/Tabs.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Tabs() {
+  return (
+    <div>Tabs</div>
+  )
+}
+
+export default Tabs
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../layouts/Header'
+import Tabs from '../components/Tabs'
+
+function Homepage() {
+  return (
+    <>
+        <Header></Header>
+        <Tabs></Tabs>
+    </>
+  )
+}
+
+export default Homepage
